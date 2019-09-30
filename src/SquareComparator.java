ce24bbfe35abe2bb69c7a9a89c66b219508a126b
muro95 <sonbena95@gmail.com>
2019-09-30 16:19:31 +0700

    first commit'

---
+++ src/SquareComparator.java
@@ -0,0 +1,11 @@
+import java.util.Comparator;
+public class SquareComparator implements Comparator<Square>{
+    @Override
+    public int compare (Square s1, Square s2){
+        if (s1.getArea() > s2.getArea()){
+            return 1;
+        }else if(s1.getArea() < s2.getArea()){
+            return -1;
+        }else return 0;
+    }
+}
